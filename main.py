b9a118d3e67223fb4fa5b6c1f7d8defe928f6e3a
Dan Haver <haverd08@gmail.com>
2023-07-02 19:01:24 +0100

    v1 completed

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# imports
 from psycopg2 import connect, DatabaseError
 from keys import HOST, USER, PASSWORD, DBNAME, PORT
 import csv
@@ -6,25 +5,33 @@ import sys
 import pandas as pd
 
 
-# take in the csv, command line? convert from xlsx to csv
 def main():
+    # check that csv arg passed
+    if len(sys.argv) != 2:
+        print("Usage: main.py <csv file>")
+        return
+    # convert .xlsx to .csv
     csv_name = sys.argv[1]
     read_file = pd.read_excel(csv_name)
     read_file.to_csv("test.csv", index=None, header=True)
     csv_name = csv_name.replace(".xlsx", ".csv")
+    # read csv
     with open(csv_name, "r") as file:
         reader = csv.DictReader(file)
         for row in reader:
-            sql_dict = row
-            keys = sql_dict.keys()
-            vals = sql_dict.values()
+            # define total keys and total values variables
+            keys = row.keys()
+            vals = row.values()
             temp = []
             for key in keys:
+                # strip of whitespaces and ? punctuation from original headers
                 key = key.replace(" ", "_").lower().strip("?")
+                # create list of keys to create new dictionary with update keys
                 temp.append(key)
+            # create new dict of updated keys and original values
             outputs = dict(zip(temp, vals))
-            print(outputs["date_ordered"])
-            for key in outputs.keys():
+            for key, value in outputs.items():
+                # initialise connection to database
                 try:
                     conn = connect(
                         host=HOST,
@@ -35,63 +42,63 @@ def main():
                     )
                     with conn:
                         with conn.cursor() as cur:
+                            # match keys to perform specific insert operation
                             match key:
                                 case "date_arrived":
                                     cur.execute(
                                         """INSERT INTO date_arrived (date_arrived) VALUES (%s)""",
-                                        (outputs["date_arrived"]),
+                                        [value],
                                     )
                                 case "date_ordered":
                                     cur.execute(
                                         """INSERT INTO date_ordered (date_ordered) VALUES (%s)""",
-                                        (outputs["date_ordered"]),
+                                        [value],
                                     )
                                 case "date_requested":
                                     cur.execute(
                                         """INSERT INTO date_requested (date_requested) VALUES (%s)""",
-                                        (outputs["date_requested"]),
+                                        [value],
                                     )
                                 case "list_price":
                                     cur.execute(
                                         """INSERT INTO list_price (list_price) VALUES (%s)""",
-                                        (outputs["list_price"]),
+                                        [value],
                                     )
                                 case "new_item":
                                     cur.execute(
                                         """INSERT INTO new_item (new_item) VALUES (%s)""",
-                                        (outputs["new_item"]),
+                                        [value],
                                     )
                                 case "po_number":
                                     cur.execute(
                                         """INSERT INTO po_number (po_number) VALUES (%s)""",
-                                        (outputs["po_number"]),
+                                        [value],
                                     )
                                 case "project_code":
                                     cur.execute(
                                         """INSERT INTO project_code (project_code) VALUES (%s)""",
-                                        (outputs["project_code"]),
+                                        [value],
                                     )
                                 case "quantity":
                                     cur.execute(
                                         """INSERT INTO quantity (quantity) VALUES (%s)""",
-                                        (outputs["quantity"]),
+                                        [value],
                                     )
                                 case "reagent":
                                     cur.execute(
                                         """INSERT INTO reagent (reagent) VALUES (%s)""",
-                                        (outputs["reagent"]),
+                                        [value],
                                     )
                                 case "researcher":
                                     cur.execute(
                                         """INSERT INTO researcher (researcher) VALUES (%s)""",
-                                        (outputs["researcher"]),
+                                        [value],
                                     )
                                 case "supplier":
                                     cur.execute(
                                         """INSERT INTO supplier (supplier) VALUES (%s)""",
-                                        (outputs["supplier"]),
+                                        [value],
                                     )
-
                             conn.commit()
                 except (Exception, DatabaseError) as error:
                     print(error)
